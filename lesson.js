ef8b7fc30ee095a36e49ef28ce77fdd53ec6ca78
MASAHIRO AMEMIYA <ame6masa6@gmail.com>
2022-08-27 16:32:43 +0900

    コンポーネントの作成

---
+++ lesson.js
@@ -0,0 +1,19 @@
+// Reactをインポートする
+import React from "react";
+
+// Lessonクラスを定義
+class Lesson extends React.component{
+  render() {
+    return() {
+      <div className='lesson-card'>
+        <div className='lesson-item'>
+          <p></p>
+          <img />
+        </div>
+      </div>
+    }
+  }
+}
+
+// Lessonクラスをexport
+export default Lesson;
